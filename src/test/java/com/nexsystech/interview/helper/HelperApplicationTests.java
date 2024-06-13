88c63538aaf76831def74a9a47d7fab5dc26b657
surya <surya@nexsys-tech.com>
2024-06-12 22:51:57 -0400

    Added mongo Configartion

--- src/test/java/com/nexsystech/interview/helper/HelperApplicationTests.java
+++ src/test/java/com/nexsystech/interview/helper/HelperApplicationTests.java
@@ -1,13 +1,13 @@
-package com.nexsystech.interview.helper;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HelperApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.nexsystech.interview.helper;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HelperApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
